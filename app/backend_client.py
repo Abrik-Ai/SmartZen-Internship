cf036be77a8094c725751b4f7da67fba3f0a65e9
Abror <dodobaevabror777@gmail.com>
2026-07-28 20:43:48 +0300

    feat/extend get_schedules with optional from/to query params

--- app/backend_client.py
+++ app/backend_client.py
@@ -2,25 +2,38 @@ import httpx
 
 from app.config import BACKEND_API_URL
 from app.models.generated import AuthTokens, Schedule
+from app.tools_call import call_with_retries
 
 
 class BackendClient:
     def __init__(self, base_url: str | None = BACKEND_API_URL) -> None:
-        if base_url is None:
+        if base_url is None: # in case the environment variable is not set
             raise ValueError("BACKEND_API_URL is not set")
         self.base_url = base_url
 
 
     def login(self, email: str, password: str) -> AuthTokens:
         url = self.base_url + "/auth/login"
-        response = httpx.post(url, json={"email": email, "password": password})
-        data = response.json()
+        response = call_with_retries(
+            lambda: httpx.post(url, json={"email": email, "password": password}, timeout=10)
+            )
+        data = response.json() # to convert JSON to a Python dictionary
         return AuthTokens.model_validate(data)
 
-    def get_schedules(self, token: str) -> list[Schedule]:
+    def get_schedules(self, token: str, 
+                      from_time: str | None = None, to_time: str | None = None
+                      ) -> list[Schedule]:
         url = self.base_url + "/schedules/me"
         headers = {"Authorization": f"Bearer {token}"}
-        response = httpx.get(url, headers=headers)
-        data = response.json()
+        #headers = {"Authorization": f"Bearer fake-token"} - to test the faked token
+        params = {}
+        if from_time is not None:
+            params["from"] = from_time
+        if to_time is not None:
+            params["to"] = to_time
+        response = call_with_retries(
+            lambda: httpx.get(url, headers=headers, params=params, timeout=10)
+            )
+        data = response.json()  
         return [Schedule.model_validate(item) for item in data]
 
